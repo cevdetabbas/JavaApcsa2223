76b37a1819031801fd065909ce5a60466620f637
cevdetabbas <cevdetabbas@gmail.com>
2022-08-19 08:50:16 -0500

    This my first commit

---
+++ MyFirstClass.java
@@ -0,0 +1,30 @@
+public class MyFirstClass
+{
+    //  after you create your project and class
+    //  you have to create your main method to run commands
+    //  in order to use shortcut you type main and press <tab>
+    public static void main(String[] args)
+    {
+//         to print out something to the screen you type System.out.println("your text here");
+//          each line of the code must end with semicolown.
+//          shortcut or System.out.println() is sout and <tab>
+        //  this double // means this is not a part of your code it is just a commment line to explain or taking note
+
+        //  this is single line comment but if you need multiple line
+
+        /*
+            this is multiple line comment until the
+            end
+        of
+        the comment
+        your can type your comment
+         */
+
+        System.out.println("My First Line Of Code");
+
+        //  another way to print on screen with System.out.print();
+        //  the difference between them println prints the text and goes to the next line but print uses the same line
+        //
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables
+{
+    public static void main(String[] args)
+    {
+        //  // DECLERAITON OF VARIABLES
+        // DataType VariableName = Value;
+        String name = "Mustafa";
+    }
+}
