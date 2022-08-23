f07ac09554383e1cc4089b7b20169109b95b2379
cevdetabbas <cevdetabbas@gmail.com>
2022-08-23 08:22:14 -0500

    Today is 08/19/2022

--- variables.java
+++ variables.java
@@ -13,17 +13,39 @@ public class variables
         short shortNumber = 4;       //  Stores whole numbers from -32,768 to 32,767
         long longNumber = 123456789; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
         double doubleNumber = 12.34; // Stores fractional numbers. Sufficient for storing 15 decimal digits
-        //  RULES FOR VARIABLES AND DATA TYPES
 
-        // you can not give reserved names for a varialbe like class etc...
-        // it should not start from a number
-        // start always with lower case, you can start with upper case but not preferable way
-        // primitives are byte short long int float double char boolean
-        // float ends with f
-        // double ends with d but it is not supposed to do.
-        // long ends with l but it is not supposed to do.
-        // char has only one character within single quote
-        // boolean has only true and false as value.
+//          RULES FOR VARIABLES AND DATA TYPES
+//
+//         you can not give reserved names for a varialbe like class etc...
+//         it should not start from a number
+//         start always with lower case, you can start with upper case but not preferable way
+//         primitives are byte short long int float double char boolean
+//         float ends with f
+//         double ends with d but it is not supposed to do.
+//         long ends with l but it is not supposed to do.
+//         char has only one character within single quote
+//         boolean has only true and false as value.
+//         variabale name rules:
+//         1. Must be started with letters
+//         2. MUST be Unique
+//         3. cannot be Java Reserved words
+//         4. cannot contain special character other $ and primitive types.
+        // 5. start always with lower case, you can start with upper case but not preferable way
+        //primitive data types: byte, short, int, long, float, double, char, boolean
+
+        //for numbers: byte, short, int, long, float, double
+
+
+
+//        ... myNumber = 9;
+//        ... myFloatNum = 8.99f;
+//        ... myLetter = 'A';
+//        ... myBool = false;
+//        ... myText = "Hello World";
+
+            String mustafaOzpay = "M.O.";
+
+        System.out.println("\"Mustafa\"");
 
 
 
