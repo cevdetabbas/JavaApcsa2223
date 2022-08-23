e581ebab227b606a1367699c88fd833f9b02d391
cevdetabbas <cevdetabbas@gmail.com>
2022-08-23 09:54:40 -0500

    Today is 08/23/2022 we learned arithmetic opeartors concatination casting (explicit and implicit casting shorthand operators += -=

---
+++ Java2_variables08192022.java
@@ -0,0 +1,53 @@
+public class Java2_variables08192022
+{
+    public static void main(String[] args)
+    {
+        //  DECLERAITON OF VARIABLES
+        // DataType VariableName = Value;
+        String name = "Mustafa";     // Text or String
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character, you can use numbers from ISCII table to define.
+        boolean myBool = true;       // Boolean
+        byte smallNumber = 3;        // Stores whole numbers from -128 to 127
+        short shortNumber = 4;       //  Stores whole numbers from -32,768 to 32,767
+        long longNumber = 123456789; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        double doubleNumber = 12.34; // Stores fractional numbers. Sufficient for storing 15 decimal digits
+
+//          RULES FOR VARIABLES AND DATA TYPES
+//
+//         you can not give reserved names for a varialbe like class etc...
+//         it should not start from a number
+//         start always with lower case, you can start with upper case but not preferable way
+//         primitives are byte short long int float double char boolean
+//         float ends with f
+//         double ends with d but it is not supposed to do.
+//         long ends with l but it is not supposed to do.
+//         char has only one character within single quote
+//         boolean has only true and false as value.
+//         variabale name rules:
+//         1. Must be started with letters
+//         2. MUST be Unique
+//         3. cannot be Java Reserved words
+//         4. cannot contain special character other $ and primitive types.
+        // 5. start always with lower case, you can start with upper case but not preferable way
+        //primitive data types: byte, short, int, long, float, double, char, boolean
+
+        //for numbers: byte, short, int, long, float, double
+
+
+
+//        ... myNumber = 9;
+//        ... myFloatNum = 8.99f;
+//        ... myLetter = 'A';
+//        ... myBool = false;
+//        ... myText = "Hello World";
+
+            String mustafaOzpay = "M.O.";
+
+        System.out.println("\"Mustafa\"");
+
+
+
+    }
+}
